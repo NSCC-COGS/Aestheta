098c59066db987fca1aeefc97564ae2389150013
donuty-party <donuty-party@users.noreply.github.com>
2021-03-17 12:23:29 -0300

    Revert model load/save to originals
    Revert loadModel() and saveModel() to their originals and remove dependency on model.py.

--- Library/core.py
+++ Library/core.py
@@ -12,23 +12,18 @@ from datetime import datetime
 import imageio
 import numpy as np
 import requests
-import shapefile
+# import shapefile # temporarily removed so our code works in colab!
 
 from matplotlib import pyplot as plt
 from scipy import ndimage
 from sklearn.ensemble import GradientBoostingClassifier
 
 # Local
-from model import Model
-from placemark import Placemark
-from tile_source import TileSource
-
-Placemark.load_config()
-TileSource.load_config()
+# None here yet!
 
 ## function to read/load shapefiles based on file name
 def shpreader(fname, show = False):
-    shp = shapefile.Reader(fname)
+    shp = shapefile.Reader(fname) # note this currently wont work!
     
     # show if show is passed as true
     if show:
@@ -45,25 +40,57 @@ def shpreader(fname, show = False):
     shp.close()
     return shp
 
+# Adapted from deg2num at https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Lon..2Flat._to_tile_numbers_2
+def tile_from_coords(lon, lat, zoom):
+    lat_rad = math.radians(lat)
+    n = 2.0 ** zoom
+    tile_x = int((lon + 180.0) / 360.0 * n)
+    tile_y = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+    return [tile_x, tile_y, zoom]
+
+# Adapted from num2deg at https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Lon..2Flat._to_tile_numbers_2
+def coords_from_tile(tile_x, tile_y, zoom):
+    n = 2.0 ** zoom
+    lon_deg = tile_x / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * tile_y / n)))
+    lat_deg = math.degrees(lat_rad)
+    return [lon_deg, lat_deg, zoom]
+
 def getTile(xyz=[0,0,0], source='google_map', show=False):
-    source2, layer = TileSource.get_alias(source)
-    tile = source2.tile(xyz, zoom=None, layer=layer)
+    '''grabs a tile of a given xyz (or lon, lat, z) from various open WMS services
+    note: these services are not meant to be web scraped and should not be accessed excessively'''
+
+    # If our coords are floats, assume we're dealing with lat and long, and
+    # convert them to tile x, y, z.
+    x, y, z = xyz
+    if isinstance(x, float) and isinstance(y, float):
+        x, y, z = tile_from_coords(x, y, z)
+
+    print(x, y, z)
+
+    if source == 'google_map':
+        url = f'http://mt.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+    elif source == 'google_sat':
+        url = f'http://mt.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+    elif source == 'osm_map':
+        url = f'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    elif source == 'mapbox_sat':
+        TOKEN = 'pk.eyJ1Ijoicm5zcmciLCJhIjoiZTA0NmIwY2ZkYWJmMGZmMTAwNDYyNzdmYzkyODQyNDkifQ.djD5YCQzikYGFBo8pwiaNA'
+        url = f'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token={TOKEN}'
+    elif source == 'esri':
+        # otiles was down so replaced with esri - a nice source
+        url = f'http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+
+    #creates a header indicating a user browser to bypass blocking, note this is not meant for exhaustive usage
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    res = requests.get(url, stream=True, headers=headers)
+    img = imageio.imread(res.content)
 
     if show:
-        plt.imshow(tile)
+        plt.imshow(img)
         plt.show()
-        
-    return tile
-    
-def getTiles_3x3(xyz=[0,0,0], source = 'google_map', show=False):
-    source2, layer = TileSource.get_alias(source)
-    tile = source2.tile_3x3(xyz, zoom=None, layer=layer)
-    
-    if show:
-        plt.imshow(tile)
-        plt.show()
-        
-    return tile
+    else:
+        return img
 
 def simpleClassifier(img_RGB, img_features, subsample = 100):
     print('training  classifier...')
@@ -85,13 +112,62 @@ def simpleClassifier(img_RGB, img_features, subsample = 100):
 
     return classModel, classes
     
-def saveModel(classModel, classes, sillyName=None):
-    model = Model(model=classModel, classes=classes)
-    model.save(label=sillyName)
+def saveModel(classModel, classes, sillyName = None):
+    #puts the classificaion model and the classes into a list 
+    model = [classModel, classes]
 
-def loadModel(name=None, model_dir='Models'):
-    model = Model.load_named_or_latest(filename=name, dir_path=model_dir)
-    return model.model, model.classes
+    #creates a string for the current time 
+    now = datetime.now()
+    uniqueString = now.strftime("%Y%m%d%H%M%S") #https://www.programiz.com/python-programming/datetime/strftime
+    
+    python_bits_user = struct.calcsize("P") * 8 #this will print 32 or 64 depending on python version
+    uniqueString += '_'+str(python_bits_user)
+
+    if sillyName:
+        uniqueString += '_'+sillyName
+
+    #saves out the model list with a name from the current time
+    filename = f'Models/simpleClassifier_{uniqueString}.aist'
+    print('saving model to',filename)
+    pickle.dump(model, open(filename, 'wb'))
+    print('complete..')
+
+def loadModel(name = None, model_dir = 'Models/'):
+    model_list = os.listdir(model_dir)
+    print(model_list)
+
+    if name == None: #loads most recent
+        maxDate = 0 
+        newest_model = None
+        print('getting most recent model')
+        for model_name in model_list:
+            model_name_hack = model_name.replace('.','_')
+            model_name_list = model_name_hack.split('_')
+            date_time = int(model_name_list[1])
+
+            python_bits_user = struct.calcsize("P") * 8 #this will print 32 or 64 depending on python version
+            python_bits_file = int(model_name_list[2])
+
+            if date_time > maxDate and python_bits_user == python_bits_file:
+                newest_model = model_name
+                maxDate = date_time
+
+            print(date_time)
+            # print(model_name.split('_'))
+            # a = re.split('_|.',model_name)
+            # print(a)
+
+    try:        
+        filename = model_dir+newest_model
+        print(filename)
+    except:
+        print(f'No Model found for {python_bits_user} bit pythion')
+        filename = input("enter model path...")
+        return
+
+    classModel, classes = pickle.load(open(filename, 'rb'))
+
+    return classModel, classes
 
 def classifyImage(img_RGB,classModel = None ,classes = None):
     if not classModel: # if no model set
@@ -116,6 +192,44 @@ def classifyImage(img_RGB,classModel = None ,classes = None):
     img_class = np.reshape(arr_label_model,arr_RGB_shape) #hard coded for 256x256 images!
     
     return img_class
+  
+#def classifyImage(img_RGB,classModel,classes):
+#    print('applying classification...')
+#    arr_RGB_shape = img_RGB.shape
+#    arr_RGB = img_RGB.reshape(-1, img_RGB.shape[-1])
+#    arr_classes_model = classModel.predict(arr_RGB)
+#    arr_label_model = classes[arr_classes_model]
+#    img_class = np.reshape(arr_label_model,arr_RGB_shape) #hard coded for 256x256 images!
+#
+#    return img_class
+
+def getTiles_3x3(xyz=[0,0,0], source = 'google_map', show=False):
+    x,y,z = xyz
+
+    # check if input are coordinates (float)
+    if isinstance(x, float) and isinstance(y, float):
+        x, y, z = tile_from_coords(x, y, z)
+
+    idx = [-1,0,1]
+    # idx = [-2-1,0,1,2]
+    img = 'Start'
+    for j in idx:
+        for k in idx:
+            print(j,k)
+            tile_img = getTile(xyz=[x+j,y+k,z], source = source, show=False)
+            #print(f"tile image shape {tile_img.shape}")
+            if img == 'Start':
+                img = np.zeros((tile_img.shape[0]*3,tile_img.shape[1]*3,tile_img.shape[2]),dtype=tile_img.dtype) 
+            x0 = (j+1)*tile_img.shape[0]
+            y0 = (k+1)*tile_img.shape[1]
+            img[y0:y0+tile_img.shape[0],x0:x0+tile_img.shape[1]] = tile_img
+ 
+    if show:
+        plt.imshow(img)
+        plt.show()
+
+    else:
+        return img
 
 def getTiles_experimental(xyz=[0,0,0], source = 'google_map', show=False):
     x,y,z = xyz
@@ -143,7 +257,19 @@ def getTiles_experimental(xyz=[0,0,0], source = 'google_map', show=False):
 
     else:
         return img
+    
+    
+# Testing Nomal Difference 
+def norm_diff(img_RGB, B1, B2, show=True):
+    img_RGB = getTile([x,y],source='google_sat', show=False)
+    ND = norm_diff(IMG_RGB, B1=1, B2=2)
 
+    if show:
+        plt.imshow(ND)
+        plt.show()
+  else:
+        return ND
+    
 def image_shift_diff(img_RGB, show=False, axis=0, distance = 1):
     img_shifted = np.roll(img_RGB,distance,axis=axis)
     img = img_shifted*1.0 - img_RGB*1.0 #multiplying by 1.0 is a lazy way yo convert an array to float
@@ -174,7 +300,7 @@ def image_convolution_RGB(img_RGB):
 
 if __name__ == '__main__':
     #for now we can put tests here!
-    if 0: #test load the wms tile
+    if 1: #test load the wms tile
         testTile = getTile()
         plt.imshow(testTile)
         plt.show()
@@ -230,11 +356,16 @@ if __name__ == '__main__':
         plt.imshow(img_RGB_c)
         plt.show()
         
-    if 0:
+    if 0: # Test image convolution with coordinate transforms.
         # Coordinates of NSCC COGS; zoom level 15
         lat, lon, z = 44.88516350846845, -65.16834212839683, 15
-
-        img_RGB = getTile(xyz=[lon, lat, z], source='google_sat')
+        
+        # Convert coords -> tile -> coords
+        x1, y1, z1 = tile_from_coords(lon, lat, z)
+        x2, y2, z2 = coords_from_tile(x1, y1, z1)
+        
+        # Feed resulting lon, lat, z to getTile.
+        img_RGB = getTile(xyz = [x2, y2, z2], source = 'google_sat')
         img = img_RGB[:,:,0]
         img_c = image_convolution(img)
 
@@ -250,7 +381,7 @@ if __name__ == '__main__':
         saveModel(classModel, classes, sillyName = 'HelloEarth100')
         print('Model saved')
 
-    if 0: #test load model
+    if 0: #test load model by name
         img_RGB = getTile(source = 'google_sat')
         classModel, classes = loadModel(name='simpleClassifier_20210302180953_64_HelloEarth100.aist')
         print('Loaded model by name')
@@ -289,10 +420,16 @@ if __name__ == '__main__':
         plt.imshow(img_class)
         plt.show()
         
-    if 0:
+    if 0: #not currently working
         # Test Placemark and TileSource. This uses relative imports, so you have
         # to run this with "python -m Library.core". "python Library/core.py"
         # will give you an error!
+        from .placemark import Placemark
+        from .tile_source import TileSource
+        
+        # Need to load configs first…
+        Placemark.load_config()
+        TileSource.load_config()
         
         # Use Google Maps as our TileSource.
         google_maps = TileSource.get('google_maps')
@@ -343,11 +480,3 @@ if __name__ == '__main__':
         
         plt.imshow(tile4)
         plt.show()
-    if 1:
-        getTile(xyz=[41, 45, 7], source='mapbox_sat', show=True)
-        getTile(xyz=[-65.17, 44.89, 15], source='osm', show=True)
-        getTile(xyz=['eiffel', 7], source='esri', show=True)
-        getTile('eiffel', show=True)
-        
-        cogs = Placemark.get('cogs')
-        getTile(cogs, show=True)
